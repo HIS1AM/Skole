e1649cd127973840f5312a682b9d8a250cd68ffc
pshis <pshis@DESKTOP-3GGBFKV>
2023-05-25 20:57:46 +0200

    Add project files.

---
+++ Skole/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Skole.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Fornavn</td>
+                    <td><asp:TextBox ID="Fornavn" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Etternavn</td>
+                    <td><asp:TextBox ID="Etternavn" runat="server"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td>KlasseID</td>
+                    <td><asp:TextBox ID="KlasseID" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <asp:Button ID="Button1" runat="server" Text="Søk" OnClick="Button1_Click" />
+                        <asp:Button ID="Button2" runat="server" Text="Søk KlasseID" OnClick="Button2_Click" />
+                    </td>
+                </tr>                           
+               
+            </table>
+            <br />
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Skole/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Skole
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=172.22.62.47;Initial Catalog=Test;User ID=hisham;Password=H1j2z3b4.04");
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(@"Data Source=172.22.62.47;Initial Catalog=Test;User ID=hisham;Password=H1j2z3b4.04"))
+            {
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT * FROM Elev WHERE Fornavn = @Fornavn AND Etternavn = @Etternavn";
+                cmd.Parameters.AddWithValue("@Fornavn", Fornavn.Text);
+                cmd.Parameters.AddWithValue("@Etternavn", Etternavn.Text);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(@"Data Source=172.22.62.47;Initial Catalog=Test;User ID=hisham;Password=H1j2z3b4.04"))
+            {
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT * FROM Elev WHERE KlasseID = @KlasseID;";
+                cmd.Parameters.AddWithValue("@KlasseID", KlasseID.Text);        
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+            }
+
+        }
+    }
+}
